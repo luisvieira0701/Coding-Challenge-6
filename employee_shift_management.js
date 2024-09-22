4cb42b032a5ba3fb2b61230e4b9deb671a6d6ddb
LuisVieira0701 <luisvieira@usf.edu>
2024-09-22 14:25:11 -0400

    Task2- Create displayEmployeeShifts function

--- employee_shift_management.js
+++ employee_shift_management.js
@@ -4,3 +4,15 @@ let employees = [
     {name: 'Jim Halpert', shifts: [{day: 'tuesday', hours: 10}, {day: 'thursday', hours: 8}]},
     {name: 'Dwight Shrute',shifts: [{day: 'friday', hours: 12}]}
 ]
+
+//Task 2 Create a Function to Display Employee Shift Details
+function displayEmployeeShift (employee) {
+      employees.forEach(worker => (worker.name, worker.shifts))
+        return (employee)
+        // return (console.log(`Employee Shift Details: ${employee}`))
+}
+
+//Testing Task 2 Function with My Employees
+let employeeShifts = displayEmployeeShift(employees)
+console.log(`Employee Shift Details:`, employeeShifts)
+
