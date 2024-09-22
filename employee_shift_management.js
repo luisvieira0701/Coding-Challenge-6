1af84e9d07069f01f2b7a40307bf703ddb439a02
LuisVieira0701 <luisvieira@usf.edu>
2024-09-22 14:01:52 -0400

    Task-1 Initialize employees with shift schedules

---
+++ employee_shift_management.js
@@ -0,0 +1,6 @@
+//Task 1 Create an Employees Array of Employee Objects
+let employees = [
+    {name: 'Michael Scott', shifts: [{day: 'monday', hours: 10}, {day: 'wednesday', hours: 8}]},
+    {name: 'Jim Halpert', shifts: [{day: 'tuesday', hours: 10}, {day: 'thursday', hours: 8}]},
+    {name: 'Dwight Shrute',shifts: [{day: 'friday', hours: 12}]}
+]
